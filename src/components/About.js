d9aa1b58bc9453d08222cf7038c5a7d963d60cfd
Yippiekaiaii <105441533+Yippiekaiaii@users.noreply.github.com>
2023-04-15 10:56:29 +0100

    Update 15_04_23 pre-upload

--- src/components/About.js
+++ src/components/About.js
@@ -8,8 +8,9 @@ const About =() => {
             <h3>About this blog</h3>
             <p>This blog was originally made as a learning project and to show my web development skills.</p>
             <p>As a demonstration of the MERN stack it uses React to connect to a NodeJS API that in turn accesses a MongoDB database.</p>
-            <p>State is handled by Redux.</p>
+            <p>It incorporates Redux to handle state management, using RTK query to fetch the data</p>
             <p>The site has full user authentication and role based authorisation.</p>
+            <p>If you would like to know more or would like to get in touch please email me <a href="mailto:peter.wilson12@notmail.co.uk">here</a></p>
         </div>
         </>
     )
